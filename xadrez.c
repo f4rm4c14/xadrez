0e2e4a70712c7b107bcb5887d3cd8feb641693eb
f4rm4c14 <rafaelmelo.farm@gmail.com>
2025-05-16 02:03:57 +0000

    xadrez aventureiro

--- xadrez.c
+++ xadrez.c
@@ -1,1 +1,45 @@
+#include <stdio.h>
 
+ int main ()
+ {
+
+    int tor = 0, bis = 0;
+    int i,j;
+    char baixo;
+   
+    while (tor <=5)
+    {
+        printf (" Torre cinco casas para direita\n");
+        tor++;
+    }
+
+    do
+    {
+        if (bis <=5)
+        {
+            printf("Diagonal para cima e a Direita\n");
+        }else {
+            printf(" teste inválido");
+        }
+        bis++;
+    } while (bis <=5);
+    
+    for (int rai=0; rai<8; rai++)
+    {
+        printf("Rainha move-se em todas as direções\n");
+    }
+
+    for ( i=1; i<=2;i++ )
+    {
+        baixo = 'c';
+            
+        for ( j=1; j<=i;j++ )
+            {
+            printf("%c ", baixo);
+            }
+            printf("\n");
+    }
+    
+
+    
+ }
